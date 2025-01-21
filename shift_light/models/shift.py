093839f914e85a3b6d2ce9bb34b12ff865e34f64
thm <thm@apik.cloud>
2025-01-21 17:36:02 +0100

    [IMP] Improve shift light module

--- shift_light/models/shift.py
+++ shift_light/models/shift.py
@@ -32,6 +32,7 @@ class ShiftShift(models.Model):
     planning_id = fields.Many2one(related="shift_template_id.planning_id", store=True)
     shift_type_id = fields.Many2one("shift.type", string="Shift Type")
     partner_id = fields.Many2one("res.partner", tracking=True)
+    partner_name = fields.Char(related='partner_id.name', related_sudo=True)
     partner_phone = fields.Char(related='partner_id.phone', related_sudo=True)
     start_time = fields.Datetime(tracking=True, index=True, required=True)
     end_time = fields.Datetime(tracking=True, required=True)
